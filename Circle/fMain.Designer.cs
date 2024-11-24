538c259cd6605dd97add112a19527e6e4f61eb62
gog1usss <greengewot@gmail.com>
2024-11-24 12:23:49 +0200

    Добавьте файлы проекта.

---
+++ Circle/fMain.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Circle
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.pnTools = new System.Windows.Forms.Panel();
+            this.btnUpFar = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnLeftFar = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRightFar = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDownFar = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnCollapse = new System.Windows.Forms.Button();
+            this.btnExpand = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnCreateNew = new System.Windows.Forms.Button();
+            this.cbCircles = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.BackColor = System.Drawing.SystemColors.Control;
+            this.pnMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(800, 450);
+            this.pnMain.TabIndex = 0;
+            this.pnMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnMain_Paint);
+            // 
+            // pnTools
+            // 
+            this.pnTools.Controls.Add(this.btnUpFar);
+            this.pnTools.Controls.Add(this.btnUp);
+            this.pnTools.Controls.Add(this.btnLeftFar);
+            this.pnTools.Controls.Add(this.btnLeft);
+            this.pnTools.Controls.Add(this.btnRightFar);
+            this.pnTools.Controls.Add(this.btnRight);
+            this.pnTools.Controls.Add(this.btnDownFar);
+            this.pnTools.Controls.Add(this.btnDown);
+            this.pnTools.Controls.Add(this.btnCollapse);
+            this.pnTools.Controls.Add(this.btnExpand);
+            this.pnTools.Controls.Add(this.btnShow);
+            this.pnTools.Controls.Add(this.btnHide);
+            this.pnTools.Controls.Add(this.btnCreateNew);
+            this.pnTools.Controls.Add(this.cbCircles);
+            this.pnTools.Controls.Add(this.label1);
+            this.pnTools.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnTools.Location = new System.Drawing.Point(471, 0);
+            this.pnTools.Name = "pnTools";
+            this.pnTools.Size = new System.Drawing.Size(329, 450);
+            this.pnTools.TabIndex = 1;
+            // 
+            // btnUpFar
+            // 
+            this.btnUpFar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnUpFar.Location = new System.Drawing.Point(136, 151);
+            this.btnUpFar.Name = "btnUpFar";
+            this.btnUpFar.Size = new System.Drawing.Size(48, 37);
+            this.btnUpFar.TabIndex = 14;
+            this.btnUpFar.Text = "↑ ↑";
+            this.btnUpFar.UseVisualStyleBackColor = true;
+            this.btnUpFar.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnUp.Location = new System.Drawing.Point(136, 194);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(48, 37);
+            this.btnUp.TabIndex = 13;
+            this.btnUp.Text = "↑";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnLeftFar
+            // 
+            this.btnLeftFar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLeftFar.Location = new System.Drawing.Point(15, 260);
+            this.btnLeftFar.Name = "btnLeftFar";
+            this.btnLeftFar.Size = new System.Drawing.Size(48, 37);
+            this.btnLeftFar.TabIndex = 12;
+            this.btnLeftFar.Text = " \t← \t←";
+            this.btnLeftFar.UseVisualStyleBackColor = true;
+            this.btnLeftFar.Click += new System.EventHandler(this.btnLeftFar_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLeft.Location = new System.Drawing.Point(69, 260);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(48, 37);
+            this.btnLeft.TabIndex = 11;
+            this.btnLeft.Text = " \t←";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btnRightFar
+            // 
+            this.btnRightFar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRightFar.Location = new System.Drawing.Point(258, 260);
+            this.btnRightFar.Name = "btnRightFar";
+            this.btnRightFar.Size = new System.Drawing.Size(48, 37);
+            this.btnRightFar.TabIndex = 10;
+            this.btnRightFar.Text = " \t→ \t→";
+            this.btnRightFar.UseVisualStyleBackColor = true;
+            this.btnRightFar.Click += new System.EventHandler(this.btnRightFar_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRight.Location = new System.Drawing.Point(204, 260);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(48, 37);
+            this.btnRight.TabIndex = 9;
+            this.btnRight.Text = " \t→";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnDownFar
+            // 
+            this.btnDownFar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDownFar.Location = new System.Drawing.Point(136, 366);
+            this.btnDownFar.Name = "btnDownFar";
+            this.btnDownFar.Size = new System.Drawing.Size(48, 37);
+            this.btnDownFar.TabIndex = 8;
+            this.btnDownFar.Text = " \t↓ \t↓";
+            this.btnDownFar.UseVisualStyleBackColor = true;
+            this.btnDownFar.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDown.Location = new System.Drawing.Point(136, 323);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(48, 37);
+            this.btnDown.TabIndex = 7;
+            this.btnDown.Text = " \t↓";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnCollapse
+            // 
+            this.btnCollapse.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCollapse.Location = new System.Drawing.Point(123, 280);
+            this.btnCollapse.Name = "btnCollapse";
+            this.btnCollapse.Size = new System.Drawing.Size(75, 37);
+            this.btnCollapse.TabIndex = 6;
+            this.btnCollapse.Text = "-";
+            this.btnCollapse.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCollapse.UseVisualStyleBackColor = true;
+            this.btnCollapse.Click += new System.EventHandler(this.btnCollapse_Click);
+            // 
+            // btnExpand
+            // 
+            this.btnExpand.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExpand.Location = new System.Drawing.Point(123, 237);
+            this.btnExpand.Name = "btnExpand";
+            this.btnExpand.Size = new System.Drawing.Size(75, 37);
+            this.btnExpand.TabIndex = 5;
+            this.btnExpand.Text = "+";
+            this.btnExpand.UseVisualStyleBackColor = true;
+            this.btnExpand.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(11, 111);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(121, 23);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Показати об\'єкт";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(11, 82);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(121, 23);
+            this.btnHide.TabIndex = 3;
+            this.btnHide.Text = "Сховати об\'єкт";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnCreateNew
+            // 
+            this.btnCreateNew.Location = new System.Drawing.Point(11, 53);
+            this.btnCreateNew.Name = "btnCreateNew";
+            this.btnCreateNew.Size = new System.Drawing.Size(121, 23);
+            this.btnCreateNew.TabIndex = 2;
+            this.btnCreateNew.Text = "Створити об\'єкт";
+            this.btnCreateNew.UseVisualStyleBackColor = true;
+            this.btnCreateNew.Click += new System.EventHandler(this.btnCreateNew_Click);
+            // 
+            // cbCircles
+            // 
+            this.cbCircles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCircles.FormattingEnabled = true;
+            this.cbCircles.Location = new System.Drawing.Point(11, 26);
+            this.cbCircles.Name = "cbCircles";
+            this.cbCircles.Size = new System.Drawing.Size(121, 21);
+            this.cbCircles.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Перелік об\'єктів";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnTools);
+            this.Controls.Add(this.pnMain);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CircleClassDemo";
+            this.pnTools.ResumeLayout(false);
+            this.pnTools.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Panel pnTools;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnCreateNew;
+        private System.Windows.Forms.ComboBox cbCircles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRightFar;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDownFar;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnCollapse;
+        private System.Windows.Forms.Button btnExpand;
+        private System.Windows.Forms.Button btnUpFar;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnLeftFar;
+        private System.Windows.Forms.Button btnLeft;
+    }
+}
+
---
+++ Circle/fMain.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circle
+{
+    public partial class fMain : Form
+    {
+        
+        public fMain()
+        {
+            InitializeComponent();
+            circles = new CCircle[100];
+        }
+
+        CCircle[] circles;
+        int CircleCount = 0;
+        int CurrentCircleIndex;
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Move(0, 10);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(0, 1);
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Expand(5);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Move(10, 0);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(0, -1);
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Move(-10, 0);
+        }
+
+        private void pnMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnCreateNew_Click(object sender, EventArgs e)
+        {
+            if (CircleCount >= 99)
+            {
+                MessageBox.Show("Досягнуто межі кількості об'єктів!");
+                return;
+            }
+            Graphics graphics = pnMain.CreateGraphics();
+            CurrentCircleIndex = CircleCount;
+            circles[CurrentCircleIndex] = new CCircle(graphics, pnMain.Width / 2, pnMain.Height / 2, 50);
+            circles[CurrentCircleIndex].Show();
+            CircleCount++;
+            cbCircles.Items.Add("Коло № " + (CircleCount - 1).ToString());
+            cbCircles.SelectedIndex = CircleCount - 1;
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Hide();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Expand(5);
+        }
+
+        private void btnCollapse_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Collapse(5);
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            circles[CurrentCircleIndex].Move(0, -10);
+        }
+
+        private void btnRightFar_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+
+        private void btnLeftFar_Click(object sender, EventArgs e)
+        {
+            CurrentCircleIndex = cbCircles.SelectedIndex;
+            if ((CurrentCircleIndex >= CircleCount) || (CurrentCircleIndex < 0))
+                return;
+            for (int i = 0; i < 100; i++)
+            {
+                circles[CurrentCircleIndex].Move(-1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+    }
+}
